73f620230139d05418f9557c103c0c1b8ea56582
Kevin TANG <tth502025390@gmail.com>
2022-03-29 00:15:29 +0800

    Add draft

--- train.py
+++ train.py
@@ -2,30 +2,71 @@ import os
 import time
 from tqdm import tqdm
 import torch
-from torch import nn, optim
+from torch import optim
+from torch.optim import lr_scheduler 
+from torch.nn.utils import clip_grad
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+basic_configs = {
+    # Data
+    'data_dir': './data',
+    'crop_size': 96,
+    'scaling_factor': 4,
+    'num_workers': 4,
 
-# Parameters
-data_dir = './data'
+    # Training
+    'device': 'cuda', # cpu or cuda
+    'batch_size': 16, 
+    'epochs':100, # number of training iterations
+    'eval_per_epochs': 5,
+    'learning_rate': 1e-4,
+    'lr_scheduler': True,
+    'lr_scheduler_parameters': {
+        'scheduler_name': None,
+        'gamma': 0.95, 
+    },
+    'grad_clip': False,
+    'save': True,
+}
 
-save = True
-batch_size = 16
-epochs = 100
-num_workers = 4
-eval_per_epochs = 5
-learning_rate = 1e-4
-learning_schedule = True
-grad_clip = False
+model_configs = {
+    
+}
 
-def run():
+# Set device here
+device = torch.device(basic_configs['device'])
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+def run(configs):
+
+    
+    
+    
     pass
 
-def train(train_data_loader, model, optimizer):
+
+def train(train_data_loader, model, optimizer, device=device, 
+        grad_clip=True, lr_scheduler=True):
 
     model.train()
 
     start_time = time.time()
     
-    for i, batch in tqdm(train_data_loader):
-        pass
+    for i, batch in tqdm(enumerate(train_data_loader)):
+        optimizer.zero_grad()
+
+        lr_imgs, hr_imgs = batch
+        lr_imgs = lr_imgs.to(device)
+        hr_imgs = hr_imgs.to(device) 
+
+        sr_imgs_pred = model(lr_imgs)
+
+        loss = model.loss(sr_imgs_pred, hr_imgs)
+
+        loss.backward()
+
+        if grad_clip:
+            pass
+
+        optimizer.step()
+
+
